559e4d7ddd0f95bbbc1fd5e80b2636b4f4447abd
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-05-05 09:01:07 +0500

    New folder added

---
+++ Modren-Cpp/01-String and Character Literals/Hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <print>
+
+using namespace std;
+
+int HelloWorld() {
+
+	print("Hello World!");
+
+	return 0;
+}
--- Modren-Cpp/Main.cpp
+++ Modren-Cpp/Main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <print>
 #include "Matrices-Calculation\Matrices-Functions.h"
+#include "01-String and Character Literals/Functioncs.h"
 
 using namespace std;
 
 int main() {
 
-
+	HelloWorld();
+	
 	return 0;
 }
